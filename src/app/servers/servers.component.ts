a9e745bbcc5460b9a9c801b1dd0fdb7d62d61ef8
Esteban <esteban_89_1@hotmail.com>
2019-01-08 03:03:22 +0100

    data binding - string interpolation and property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer: boolean = false;
+
+  constructor() { 
+    //After 2 seconds change allowNewServer value to simulate 
+    //how i can change dinamically the attribute value
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
 }
+
+
